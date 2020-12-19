9ac62a63872f7bb3aca7a5777cf146561bf9ccb4
北代櫂 <kitadaikai@kitadaikainoMacBook-ea.local>
2020-12-20 00:11:14 +0900

    feat: 回答ボタンの追加と開始ボタンの修正

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
+import React, { useState } from 'react';
 import './App.css';
 import ExecuteButton from './components/ExecuteButton';
 
-const test = (props) => {
-  console.log("test");
-} 
+// let phase = 1;
+
+const yes = () => {
+  console.log("yes");
+}
+const no = () => {
+  console.log("no");
+}
+
 
 function App() {
+
+  const [phase, setPhase] = useState(1)
+
+  const test = () => {
+    setPhase(phase + 1);
+    console.log(phase)
+  }
+
   return (
     <div>
-      <ExecuteButton func={test} title='診断開始'/>
+      { phase === 1 
+        ? <ExecuteButton func={test} title='診断開始' />
+        : null
+      }
+      { phase === 2
+     ?<ExecuteButton func={yes} title='Yes' />
+    :null
+    }
+    {phase === 2
+       ?<ExecuteButton func={no} title='No' />
+       :null
+       }
+    
+    
+
     </div>
   );
 }
+
 
 export default App;
