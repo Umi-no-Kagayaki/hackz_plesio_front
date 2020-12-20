087a6f0fc915078c3142175440004c76e4d92cde
FukeKazki <kazkichi0906@gmail.com>
2020-12-20 15:14:19 +0900

    Update: サーバーへの通信を実装

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 import './App.css';
 import QuestionView from './components/QuestionView'
@@ -8,10 +9,18 @@ import Calculate from './components/Calculate.jsx'
 
 function App() {
 
-
   const [phase, setPhase] = useState(1)
   const [answers, setAnswers] = useState([null, null, null, null, null, null, null, null, null, null]) // 回答結果を保存するstate
 
+  useEffect(() => {
+    axios.get(`https://1a7ced62262e.ngrok.io/controller`)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  }, [])
 
   const test = () => {
     setPhase(phase + 1);
